23d4ae24bb9c055ea4de908cef2874705d6d38b4
Ananthasireesh.ta <ananthasireesh.ta@cilalabs.com>
2021-10-25 20:07:58 +0530

    Arithmetic_ops

---
+++ Basic_ops/arithmetic.py
@@ -0,0 +1,28 @@
+""" This code will introduce you basic arthemetic operations in python 
+    we will learn arthematic operations on numerical variables """
+
+#Arthemetic operations on numerical variables     
+a=10 
+b=20 
+
+#addition in python is given by operator "+"
+c=a+b 
+print("Addition of two variables a and b is:",c)
+#subtraction in python is given by operator "-"
+d=a-b
+print("subtraction  of two variables a and b is:",d)
+#Division is python is given by operator "/" (this result will give only Quotient)
+e=a/b
+print("quotient of divison of two variables a and b is:",e)
+#To get only integer part of Quotient we can use "//" operator
+f=a//b
+print("Integer quotient of divison of two variables a and b is:",f)
+#To get the reminder of divison we can use "%" operator 
+g=a%b 
+print("Reminder of divison of two variables a and b is:",g)
+# to get the power of we can use operator "**"
+h=a**b
+print("power of a to b is:",h)
+#to get multiplication of two numbers we can use operator of "*"
+i=a*b
+print("multiplication result of a and b is:",i)
