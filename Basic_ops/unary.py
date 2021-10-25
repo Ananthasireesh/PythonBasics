c0e60a2759b081ea9f38daec371c3d79965e82af
Ananthasireesh.ta <ananthasireesh.ta@cilalabs.com>
2021-10-25 20:57:29 +0530

    unary ops

---
+++ Basic_ops/unary.py
@@ -0,0 +1,11 @@
+""" This code will introduce you unary operators in python 
+    Unary operators are basically used to assign a sign to a variable """
+
+""" There are only two unary operators Unary Plus(+) and Unary(-)"""
+a=5
+print("value of +a is:",+a)
+print("value of -a is:",-a)
+
+b=-3
+print("value of +b is:",+b)
+print("value of -b is:",-b)
